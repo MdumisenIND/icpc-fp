fb03cd9216aac47aa23d145422c11fd57ea2779a
MdumisenIND <36520179+MdumisenIND@users.noreply.github.com>
2020-06-01 23:37:13 +0200

    working

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -1,12 +1,53 @@
-﻿module ICPC
+module ICPC
 open System
 
-let commaSprinkler input =
-    failwith "Not implemented"
+let commaSprinkler (input:string) = 
+    let rec mined (input:string) count = 
+        match count=input.Length-1 with
+        | true -> Some input
+        | false ->
+            match Char.IsLower(input,count) || input.[count]=',' || input.[count]=' ' || input.[count]='.' with
+            |true -> 
+                match (Char.IsLower(input,count) || (input.[count]=' ' && Char.IsLower(input,count+1)) || (input.[count]=',' && input.[count+1]=' ') || (input.[count]='.' && input.[count+1]=' ')) with
+                | true -> mined input (count+1)
+                | false -> None
+            | false -> None 
+    match (input.Length>1) with
+    | true -> 
+        match (input.[input.Length-1]='.') && Char.IsLower(input,0) with
+        | true ->(mined input 0)
+            //let mine = used.Split(' ') |> List.ofSeq
+            //let beforeComma = mine |> List.filter (fun c -> c.[c.Length-1]=',') |> List.map (fun c -> c.[0..(c.Length-2)) 
+        | false -> None
+    | false -> None
 
 let rivers input =
-    failwith "Not implemented"
-
+    let rec mined (xs: string list)=
+        match xs with
+        | [] -> true
+        | x::rest ->
+            match (x.Length)<81 with
+            | true -> mined rest
+            | false -> false
+    let rec checker (input: string) count =
+        match count=input.Length with
+        | true -> 
+            let changed = input.Split(' ') |> List.ofSeq
+            match changed.Length>1 with
+            | true ->   
+                match mined changed with
+                | true->
+                    match input.[0]<>' ' || input.[input.Length-1]<>' ' with
+                    | true -> None
+                    | false -> Some input
+                | _ -> None
+            | false -> None
+        | false ->
+            match (Char.IsLetter(input,count) || (input.[count]=' ')) with
+            | true -> checker input (count+1)
+            | false -> None
+    checker input 0  
+        
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
